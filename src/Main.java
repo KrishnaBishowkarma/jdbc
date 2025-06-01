59351cd2f4f7cfce1fc38bbd64720b32e429ba3a
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2025-06-01 07:53:43 +0545

    feat: import java.sql as the first step

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.sql.*;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+        /*
+        7 steps for JDBC:
+            1. import package (java.sql)
+            2. load and register
+            3. create connection
+            4. create statement
+            5. execute statement
+            6. process
+            7. close
+         */
+
 
     }
 }
