3a3d46cb8f0360642034aa0dc119740b24e02e9b
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2025-06-02 22:59:26 +0545

    refactor: change query to fetch all students and loop through results

--- src/Main.java
+++ src/Main.java
@@ -22,7 +22,7 @@ public class Main {
         String url = "jdbc:postgresql://localhost:5432/demo";
         String uname = "postgres";
         String pass = "admin";
-        String sql = "select sname from student where sid = 2";
+        String sql = "select * from student";
         String sname = "sname";
 
 
@@ -31,9 +31,16 @@ public class Main {
 
         Statement st = con.createStatement();
         ResultSet rs = st.executeQuery(sql);
-        rs.next();
-        String name = rs.getString(sname);
-        System.out.println("Name of the student is: " + name);
+
+//        rs.next();
+//        String name = rs.getString(sname);
+//        System.out.println("Name of the student is: " + name);
+
+        while (rs.next()){
+            System.out.print(rs.getInt(1) + ", ");
+            System.out.print(rs.getString(2) + ", ");
+            System.out.println(rs.getInt(3));
+        }
 
         con.close();
         System.out.println("Connection Closed!");
